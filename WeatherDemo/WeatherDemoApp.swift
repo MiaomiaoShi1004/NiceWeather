e5c667f50135bbd59e110819306b855b9004c4a2
MiaomiaoShi1004 <123462116+MiaomiaoShi1004@users.noreply.github.com>
2023-11-13 02:10:11 +0000

    Initial Commit

---
+++ WeatherDemo/WeatherDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDemoApp.swift
+//  WeatherDemo
+//
+//  Created by Miaomiao Shi on 13/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
