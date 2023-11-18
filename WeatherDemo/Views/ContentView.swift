d38aa2d38f6731bf9de7bd4e159970ccae1a3818
MiaomiaoShi1004 <simoneshi1004@outlook.com>
2023-11-18 15:49:59 +0000

    create WelcomeView

--- WeatherDemo/Views/ContentView.swift
+++ WeatherDemo/Views/ContentView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
+    // initiate our location manager
+    @StateObject var locationManager = LocationManager()
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
