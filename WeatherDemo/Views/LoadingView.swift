35662206345965c298ef9d97c77330fead82a8c4
MiaomiaoShi1004 <simoneshi1004@outlook.com>
2023-11-19 01:44:36 +0000

    Create LoadingView and WelcomeView

---
+++ WeatherDemo/Views/LoadingView.swift
@@ -0,0 +1,18 @@
+//
+//  LoadingView.swift
+//  WeatherDemo
+//
+//  Created by Miaomiao Shi on 19/11/2023.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoadingView()
+}
