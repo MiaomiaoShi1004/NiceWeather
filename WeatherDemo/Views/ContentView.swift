440b387be822f5867b3057534761ff8d5d41b318
MiaomiaoShi1004 <simoneshi1004@outlook.com>
2023-11-19 01:48:22 +0000

    test-change

--- WeatherDemo/Views/ContentView.swift
+++ WeatherDemo/Views/ContentView.swift
@@ -13,12 +13,21 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            // Check if there is a location available in locationManager
+            if let location = locationManager.location {
+                Text("Your corordinates are:\(location.longitude),\(location.latitude)")
+            } else {
+                if locationManager.isLoading {
+                    LoadingView()
+                } else {
+                    // if locationManger is not loading and there's no location available.
+                    WelcomeView()
+                        .environmentObject(locationManager)
+                }
+            }
         }
-        .padding()
+        .background(Color(hue: 0.656, saturation: 0.787, brightness: 0.354))
+        .preferredColorScheme(.dark)
     }
 }
 
